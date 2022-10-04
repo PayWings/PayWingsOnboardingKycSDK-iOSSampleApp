0acbaee5ee8c66027405b69966c1063c932ab402
tjasajan <you@example.com>
2022-10-04 23:26:15 +0200

    KYC SDK v5.0.0

---
+++ Pods/PayWingsOnboardingKYC/PayWingsOnboardingKYC.framework/Headers/PayWingsOnboardingKYC.h
@@ -0,0 +1,19 @@
+//
+//  PayWingsOnboardingKYC.h
+//  PayWingsOnboardingKYC
+//
+//  Created by tjasa on 29/07/2020.
+//  Copyright © 2020 Intech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PayWingsOnboardingKYC.
+FOUNDATION_EXPORT double PayWingsOnboardingKYCVersionNumber;
+
+//! Project version string for PayWingsOnboardingKYC.
+FOUNDATION_EXPORT const unsigned char PayWingsOnboardingKYCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayWingsOnboardingKYC/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-PayWingsOnboardingKYC-SampleApp/Pods-PayWingsOnboardingKYC-SampleApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PayWingsOnboardingKYC_SampleApp : NSObject
+@end
+@implementation PodsDummy_Pods_PayWingsOnboardingKYC_SampleApp
+@end
